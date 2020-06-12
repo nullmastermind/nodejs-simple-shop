9d1d7a479830c8371c5be9de5cedf0ad7702919b
0x317374 <0x317374@gmail.com>
2020-06-12 15:40:22 +0700

    :beers:

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function NotFound() {
+    return (
+        <React.Fragment>
+            <head>
+                <title>404 Page Not Found</title>
+            </head>
+            <main>404 Page Not Found</main>
+        </React.Fragment>
+    );
+}
