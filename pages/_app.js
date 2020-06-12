af9e8e5849f452d19b4558696ee38e8499dfbe3b
0x317374 <0x317374@gmail.com>
2020-06-13 00:40:09 +0700

    :beers:

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "../styles/styles.scss";
+import axios from "../utils/axios";
 
 export default function MyApp(props) {
     return <props.Component {...props.pageProps} />;
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+    let pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+    pageProps.testData = (await axios.get("/api/hello")).data;
+    return { pageProps };
+};
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import React from "react";
-import axios from "../utils/axios";
 
 export default function Home(props) {
     return (
@@ -8,14 +7,7 @@ export default function Home(props) {
             <Head>
                 <title>Shop</title>
             </Head>
-            <main>{props.name}</main>
+            <main>{props.testData.name}</main>
         </React.Fragment>
     );
-}
-
-export async function getStaticProps() {
-    let data = await axios.get("/api/hello");
-    return {
-        props: data.data,
-    };
 }
